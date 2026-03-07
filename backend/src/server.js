5e2d3a62c327afab9010f6b38e34c4d456e9e4cb
Swagat <pattanayakswagat2004@gmail.com>
2026-03-07 22:32:19 +0530

    fix: update CORS origin URL and add Vercel configuration file

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://video-calling-interview-platform-us-delta.vercel.app/"
+    "https://video-calling-interview-platform-us.vercel.app/"
   ],
   credentials: true
 }));//credentials true allows browser to include cookies to be sent with requests
